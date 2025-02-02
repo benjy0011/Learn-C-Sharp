d36d2fb3cb930bc75400e91ada38397989698ab2
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2025-02-02 23:09:20 +0800

    Add project files.

---
+++ ConsoleApp.VariableDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// Different Datatypes
+
+/*
+    text - string
+    integers - int
+    decimal - double, float, decimal
+    logical - bool
+ */
+
+string name = "Benjy";
+
+Console.WriteLine("I am " + name); // String concatenation
+Console.WriteLine($"They call me {name}"); // String interpolation
+Console.WriteLine("I was given the name {0}", name); // Foramtted string
+
+
+int age = 24;
+Console.WriteLine("My age is: " + age);
+
+bool isRetired = false;
+Console.WriteLine("Am I retireed? " +  isRetired);
+
