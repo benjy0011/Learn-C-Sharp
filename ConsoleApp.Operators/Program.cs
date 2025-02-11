a2c149ac7e6ee4fc3edcfa09c6d596bc2ea09ba3
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2025-02-11 17:55:14 +0800

    Added Operators demo and enhanced user input demo

---
+++ ConsoleApp.Operators/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Please enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int num2 = 0;
+Console.Write("Please eenter the second number: ");
+string numberEntered = Console.ReadLine();
+num2 = Convert.ToInt32(numberEntered);
+
+
+/*
+    Math Operations and Operators
+*/
+
+// Add numbers
+int sum = (int)(num1 + num2);
+
+// Multiply
+int product = num1 * num2;
+
+// Division
+int quotient = num1 / num2;
+
+// Subtraction
+int difference = num1 - num2;
+
+// Modulus
+int mod = num1 % num2;
+
+Console.WriteLine("\n****************** Math Results ******************");
+Console.WriteLine($"Sum: {sum}");
+Console.WriteLine($"Difference: {difference}");
+Console.WriteLine($"Product: {product}");
+Console.WriteLine($"Quotient: {quotient}");
+Console.WriteLine($"Modulus: {mod}");
+
+
+/*
+    Logic Operations and Operators
+*/
+
+bool isGreaterThan = num1 > num2;
+bool isLessThan = num1 < num2;
+bool isEqualTo = num1 == num2;
+bool isGreaterOrEqual = num1 >= num2;
+bool isLesserOrEqual = num1 <= num2;
+bool isNotEqual = num1 != num2;
+
+Console.WriteLine("\n****************** Logic Results ******************");
+Console.WriteLine($"Is Greater Than: {isGreaterThan}");
+Console.WriteLine($"Is Less Than: {isLessThan}");
+Console.WriteLine($"Is Equal To: {isEqualTo}");
+Console.WriteLine($"Is Greater Or Equal: {isGreaterOrEqual}");
+Console.WriteLine($"Is Lesser Or Equal: {isLesserOrEqual}");
+Console.WriteLine($"Is Not Equal: {isNotEqual}");
+
+
+/*
+    Assignment Operations and Operators
+ */
+Random random = new Random();
+int randomValue = random.Next(1, 11);
+
+
+Console.WriteLine("\n****************** Assignment Results ******************");
+Console.WriteLine();
+num1 += randomValue;
+Console.WriteLine($"Num 1 increased by {randomValue}: {num1}");
+num1 -= randomValue;
+Console.WriteLine($"Num 1 reduced by {randomValue}: {num1}");
+num1 /= randomValue;
+Console.WriteLine($"Num 1 divided by {randomValue}: {num1}");
+num1 %= randomValue;
+Console.WriteLine($"Num 1 mod by {randomValue}: {num1}");
+num1 *= randomValue;
+Console.WriteLine($"Num 1 multiplied by {randomValue}: {num1}");
--- ConsoleApp.UserInputDemo/Program.cs
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -1,20 +1,32 @@
 ﻿// Declare variables
 using System.Xml.Linq;
 
-string firstName = string.Empty;
+string? firstName = string.Empty;
 string lastName = string.Empty;
-int age = 0;
+int age; // default int is 0
 int retirementAge = 65;
+decimal salary = 0;
+char gender = char.MinValue;
+bool working = false;
 
 // Prompt the user for input
-Console.WriteLine("Please enter your first name");
+Console.Write("Please enter your first name: ");
 firstName = Console.ReadLine();
 
-Console.WriteLine("Please enter your last name");
+Console.Write("Please enter your last name: ");
 lastName = Console.ReadLine();
 
-Console.WriteLine("Please enter your age");
+Console.Write("Please enter your age: ");
 age = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter your salary: ");
+salary = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Please enter your gender (M or F): ");
+gender = Convert.ToChar(Console.ReadLine());
+
+Console.Write("Are you working? (true or false): ");
+working = Convert.ToBoolean(Console.ReadLine());
 
 // Process the data
 int workingYearsRemaining = retirementAge - age;
@@ -22,5 +34,8 @@ int workingYearsRemaining = retirementAge - age;
 // Output the results to the user
 Console.WriteLine($"Full name: {firstName} {lastName}");
 Console.WriteLine($"Age: {age}");
-Console.WriteLine($"Working years remaining: {workingYearsRemaining}");
+Console.WriteLine($"Your Salary is: {salary}");
+Console.WriteLine($"Your Gender is: {gender}");
+Console.WriteLine($"You Are Employed: {working}");
+Console.WriteLine($"Number of working years remaining: {workingYearsRemaining}");
 
