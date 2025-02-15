1edffe9498c7f691333106d9223190e557a87d89
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2025-02-16 02:24:59 +0800

    Explored conditions and decisions statements

---
+++ ConsoleApp.ConditionsAndDecisions/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Please enter student's grade: ");
+int grade = Convert.ToInt32(Console.ReadLine());
+
+
+// Decide to print pass or fail based on input
+if(grade > 50)
+{
+    Console.WriteLine("Student has passed");
+}
+else
+{
+    Console.WriteLine("Student has failed");
+    Console.WriteLine("Please recommend student to student affair's office");
+}
+
+Console.WriteLine("Thankyou for using this program");
+
+
+Console.WriteLine("*************************************");
+
+if(grade < 50)
+{
+    Console.WriteLine("Student has failed - F");
+}
+else if (grade >= 50 && grade <= 65)
+{
+    Console.WriteLine("C+");
+}
+else if (grade <= 85)
+{
+    Console.WriteLine("B+");
+}
+else
+{
+    Console.WriteLine("A - Good job");
+}
+
+Console.WriteLine("*************************************");
+
+string passStatus = grade >= 50 ? "Pass" : "Fail";
+
+Console.WriteLine("*************************************");
+
+Console.Write("Please enter the day of the week: ");
+int dayOfTheWeek = Convert.ToInt32(Console.ReadLine());
+switch (dayOfTheWeek)
+{
+    case 1:
+        Console.WriteLine("Sunday");
+        break;
+    case 2:
+        Console.WriteLine("Monday");
+        break;
+    case 3:
+        Console.WriteLine("Chewsday");
+        break;
+    case 4:
+        Console.WriteLine("Wednesday");
+        break;
+    case 5:
+        Console.WriteLine("Thursday");
+        break;
+    case 6:
+        Console.WriteLine("Friday");
+        break;
+    case 7:
+        Console.WriteLine("Saturday");
+        break;
+    default:
+        Console.WriteLine("Error");
+        break;
+}
---
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to the sample calculator");
+
+// Prompt for user input
+Console.WriteLine("Please select an operation");
+Console.WriteLine("1. Addition");
+Console.WriteLine("2. Subtraction");
+Console.WriteLine("3. Multiplication");
+Console.WriteLine("4. Division");
+int choice = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+/*
+if (choice == 1)
+{
+
+}
+else if (choice == 2)
+{
+
+}
+else if (choice == 3)
+{
+
+}
+else if (choice == 4)
+{
+
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
+*/
+
+int answer = 0;
+switch (choice)
+{
+    case 1:
+        answer = num1 + num2;
+        break;
+    case 2:
+        answer = num1 - num2;
+        break;
+    case 3:
+        answer = num1 * num2;
+        break;
+    case 4:
+        answer = num1 / num2;
+        break;
+    default:
+        Console.WriteLine("Invalid choice");
+        break;
+}
+
+// Print output
+Console.WriteLine($"The result is: {answer}");
