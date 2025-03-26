7270613e1328794ae4016e00653c35027c81d981
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2025-03-27 00:25:15 +0800

    Arrays and Lists

---
+++ ConsoleApp.Arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+// Declare Fixed Size Array
+int[] grades = new int[5]; // [num] is the size
+// Index : 0, 1, 2, 3, 4
+
+
+// Add values to fixed size array
+grades[0] = 45;
+grades[1] = 25;
+grades[2] = 38;
+grades[3] = 45;
+grades[4] = 54;
+
+for (int i = 0; i < grades.Length; i++)
+{
+    Console.Write("Enter Grade: ");
+    grades[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+// Print values in Fixed size Array 
+Console.WriteLine("The Grades you have entered are: ");
+foreach (var grade in grades)
+{
+    Console.WriteLine(grade);
+}
+
+// Declare Variable Sized Array
+string[] studentNames = { "Newton", "Joshua", "Bryant" };
+
+// Add values to Variable Sized Array
+for (int i = 0; i < studentNames.Length; i++)
+{
+    Console.WriteLine(studentNames[i]);
+}
---
+++ ConsoleApp.Lists/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// Declare a List
+List<int> grades = new List<int>();
+// var grades = new List<int>();  // also can
+
+// Add values to list 
+grades.Add(45);
+
+// or 
+//List<int> gradesList<int> grades = [45];
+
+
+Console.WriteLine(grades[0]);
+
+
+// Prefer to use while - no limit
+int grade = 0;
+do
+{
+    Console.Write("Enter Grade: ");
+    grade = Convert.ToInt32(Console.ReadLine());
+    if (grade != -1)
+    {
+        grades.Add(grade);
+    }
+} while (grade != -1);
+
+
+// Print values in list - foreach
+Console.WriteLine("Printing grades via foreach");
+foreach (int g in grades)
+{
+    Console.WriteLine(g);
+}
